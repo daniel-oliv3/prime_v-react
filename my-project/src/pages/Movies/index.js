a8afda2e91a369fde803d560e4a99fa75f7a3dbb
Daniel Oliveira <danielsapup3@gmail.com>
2021-09-29 15:06:05 -0300

    Movies

---
+++ my-project/src/pages/Movies/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function Movies() {
+    return (
+        <View>
+            <Text>Tela de Filmes</Text>
+        </View>
+    )
+}
+
+export default Movies;
